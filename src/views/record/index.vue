b2be925655835c9597b13734f3a8147f96586467
k147324 <108058549+k147324@users.noreply.github.com>
2024-11-13 02:54:08 +0800

    Update index.vue

--- src/views/record/index.vue
+++ src/views/record/index.vue
@@ -3,11 +3,18 @@
     <div class="record">
       <div class="record-header">紀錄</div>
       <el-scrollbar class="scroll-area">
-        <div>
-          <div v-for="(count, noticeInfo) in rec" :key="index">
-            {{ noticeInfo }}: {{ count }} 次
-          </div>
-        </div>
+        <el-table :data="tableData" style="width: 100%">
+          <el-table-column
+            prop="noticeInfo"
+            label="學生 ID"
+            width="180"
+          ></el-table-column>
+          <el-table-column
+            prop="count"
+            label="作弊次數"
+            width="180"
+          ></el-table-column>
+        </el-table>
         <el-button @click="clearRecords" type="danger" class="clear-button">
           清空紀錄
         </el-button>
@@ -19,37 +26,38 @@
 import { useStore } from "vuex";
 import { computed, onMounted, ref } from "vue";
 import WebsocketService from "../../service/websocket";
+
+const tableData = ref([]);
+
+const handleRecord = (event) => {
+  if (Array.isArray(event)) {
+    const recordMap = {};
+
+    event.forEach((record) => {
+      const { notice_info } = record;
+      recordMap[notice_info] = (recordMap[notice_info] || 0) + 1;
+    });
+
+    tableData.value = Object.entries(recordMap).map(([noticeInfo, count]) => ({
+      noticeInfo,
+      count,
+    }));
+  } else {
+    console.error("Event data is not an array");
+  }
+};
+
+const clearRecords = () => {
+  WebsocketService.sendClearRequest(userId.value.userId);
+  tableData.value = [];
+};
+
 onMounted(() => {
   WebsocketService.addEventListener("record", handleRecord);
   const plainUserId = userId.value.userId;
   WebsocketService.sendRecordRequest(plainUserId);
 });
 
-const rec = ref({});
-const handleRecord = (event) => {
-  console.log(event);
-
-  if (Array.isArray(event)) {
-    const newRecord = { ...rec.value };
-
-    event.forEach((record) => {
-      const { notice_info } = record;
-      if (newRecord[notice_info]) {
-        newRecord[notice_info]++;
-      } else {
-        newRecord[notice_info] = 1;
-      }
-    });
-
-    rec.value = newRecord;
-  } else {
-    console.error("Event data is not an array");
-  }
-};
-const clearRecords = () => {
-  WebsocketService.sendClearRequest(userId.value.userId);
-  rec.value = {};
-};
 const store = useStore();
 const userId = computed(() => store.getters["user/getUser"]);
 </script>
